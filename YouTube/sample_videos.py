539ef30797c18cef696973fa0135cf88c3bd820b
Ben Burgess <baburges@umich.edu>
2020-04-13 14:51:34 -0400

    Add video viewer

--- YouTube/sample_videos.py
+++ YouTube/sample_videos.py
@@ -4,11 +4,13 @@ import googleapiclient.discovery
 import googleapiclient.errors
 import pandas as pd
 import json
-from pandas.io.json import json_normalize  
+import webbrowser
+from os import path
+from pandas import json_normalize  
 
 CLIENT_SECRETS_FILE = "client_secrets.json"
 RANKINGS_DIRECTORY = "./Category Rankings"
-
+NUMBER_OF_VIDEOS_TO_SAMPLE_PER_CATEGORY = 10
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 def setupAPI():
@@ -75,11 +77,74 @@ def processResponse(jsonReponse):
     dfResponse["id"] = items["id"]
     return dfResponse
 
+def questionnaire(id):
+    print("\n\nVideo: " + id + "\n")
+    print("1. Child Directed")
+    print("2. Mixed Content")
+    print("3. Not Child Directed\n")
+
+    intTag = None
+    while(True):
+      tag = input("Tag: ")
+      try:
+        if int(tag) in range(1,4):
+          intTag = int(tag)
+          break
+        else:
+          print("Error!!! Value out of range")
+      except:
+        print("Error!!! Integer not entered")
+
+    return intTag
+
+
+def generateViewerHTML(id, description):
+    viewerHTML = """<h1>{}</h1><iframe width="560" height="315" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> <br> <br> <p>{}</p>""".format(id, id, description) 
+    
+    f = open("viewer.html", "w")
+    f.write(viewerHTML)
+    f.close()
+
+    webbrowser.open("./viewer.html", new=2)
+    
+
+def viewer(mpVideos):
+    if path.exists('result.json'):
+      result = pd.read_json(r'result.json')
+    else:
+      result = pd.DataFrame()
+      result["id"] = None
+      result["tag"] = None
+
+    ids = list()
+    descriptions = list()
+    for category in mpVideos:
+      for index in range(0, NUMBER_OF_VIDEOS_TO_SAMPLE_PER_CATEGORY):
+        ids.append(category["id"][index])
+        descriptions.append(category["description"][index])
+    
+    for index in range(len(ids)):
+      id = ids[index]
+      description = descriptions[index]
+      
+      if result["id"].str.contains(id).any():
+        continue
+
+      generateViewerHTML(id, description)
+
+      tag = questionnaire(id)
+
+      result = result.append({'id':id, 'tag':tag}, ignore_index=True)
+
+      result.to_json(r'result.json')
+
+
+
 def main():
-    youtubeAPI = setupAPI()
-    
+    #youtubeAPI = setupAPI()
+
     mpVideos = loadMostPopularVideos()
-    print(mpVideos)
-    
+    viewer(mpVideos)
+
 if __name__ == "__main__":
     main()
